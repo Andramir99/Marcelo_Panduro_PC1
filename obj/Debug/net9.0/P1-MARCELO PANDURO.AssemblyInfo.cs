a261e7c98f16286af6378784a7566f636761f1c9
MarceloPA <genialixzz@gmail.com>
2025-03-29 00:19:22 -0500

    se creo la funcionalidad del tipo de cambio

--- obj/Debug/net9.0/P1-MARCELO PANDURO.AssemblyInfo.cs
+++ obj/Debug/net9.0/P1-MARCELO PANDURO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P1-MARCELO PANDURO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b101ee700e7a98fbd8967b87f023416bbee766f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("P1-MARCELO PANDURO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P1-MARCELO PANDURO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
